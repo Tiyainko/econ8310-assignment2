b4c7e729e4309da1bf16f77c060519a720cdcda2
Tiyainko <138532412+Tiyainko@users.noreply.github.com>
2026-03-11 21:34:55 -0500

    assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 
 train = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/Econ8310/master/AssignmentData/assignment3.csv")
 test = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/Econ8310/master/AssignmentData/assignment3test.csv")
@@ -7,14 +7,22 @@ test = pd.read_csv("https://raw.githubusercontent.com/dustywhite7/Econ8310/maste
 def prepare_data(df):
     df = df.copy()
     dt = pd.to_datetime(df["DateTime"])
-    df["hour"] = dt.dt.hour
-    df["dayofweek"] = dt.dt.dayofweek
+    df["year"] = dt.dt.year
     df["month"] = dt.dt.month
     df["day"] = dt.dt.day
+    df["dayofweek"] = dt.dt.dayofweek
+    df["dayofyear"] = dt.dt.dayofyear
+    df["quarter"] = dt.dt.quarter
+    df["hour"] = dt.dt.hour
     df["minute"] = dt.dt.minute
     df["is_weekend"] = (dt.dt.dayofweek >= 5).astype(int)
-    df = df.drop(columns=["id", "DateTime"])
-    return df
+    df["is_morning"] = ((dt.dt.hour >= 5) & (dt.dt.hour < 11)).astype(int)
+    df["is_lunch"] = ((dt.dt.hour >= 11) & (dt.dt.hour < 15)).astype(int)
+    df["is_afternoon"] = ((dt.dt.hour >= 15) & (dt.dt.hour < 17)).astype(int)
+    df["is_dinner"] = ((dt.dt.hour >= 17) & (dt.dt.hour < 22)).astype(int)
+    df["is_late"] = ((dt.dt.hour >= 22) | (dt.dt.hour < 5)).astype(int)
+    df = df.drop(columns=["DateTime", "id"], errors="ignore")
+    return pd.get_dummies(df, drop_first=False)
 
 train_prep = prepare_data(train)
 test_prep = prepare_data(test)
@@ -23,14 +31,19 @@ X_train = train_prep.drop(columns=["meal"])
 y_train = train_prep["meal"].astype(int)
 
 X_test = test_prep.drop(columns=["meal"], errors="ignore")
+X_test = X_test.reindex(columns=X_train.columns, fill_value=0)
 
-model = RandomForestClassifier(
-    n_estimators=300,
-    min_samples_leaf=2,
-    random_state=42,
-    n_jobs=-1
+model = XGBClassifier(
+    n_estimators=400,
+    max_depth=5,
+    learning_rate=0.05,
+    subsample=0.9,
+    colsample_bytree=0.9,
+    objective="binary:logistic",
+    eval_metric="logloss",
+    random_state=42
 )
 
 modelFit = model.fit(X_train, y_train)
 
-pred = pd.Series(modelFit.predict(X_test)).astype(int)
+pred = pd.Series((modelFit.predict_proba(X_test)[:, 1] >= 0.5).astype(int))
